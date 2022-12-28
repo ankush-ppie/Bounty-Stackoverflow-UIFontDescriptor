4288ba0fce0dbf7d62ce8f26a41cd857c99b8a94
ankush-est <ankush.lokhandey@gmail.com>
2022-12-28 22:37:24 +0530

    Add comments

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // test all fonts - Avenir Next
         var fonts: [String] = UIFont.fontNames(forFamilyName: "Avenir Next")
+        print(fonts)
         var i: CGFloat = 0;
         for font in fonts {
             let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 250 + i))
@@ -21,6 +23,7 @@ class ViewController: UIViewController {
             self.view.addSubview(label)
             i += 100;
         }
+        // test boldDescriptor font
         let boldDescriptor = UIFontDescriptor(fontAttributes: [
             UIFontDescriptor.AttributeName.family: "Avenir Next",
             UIFontDescriptor.AttributeName.traits: [

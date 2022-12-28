329c77776752525d3bdcaa67fb549b0ad7967cf5
ankush-est <ankush.lokhandey@gmail.com>
2022-12-28 22:03:15 +0530

    Implement Font example

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,7 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var fonts: [String] = UIFont.fontNames(forFamilyName: "Avenir Next")
+        var i: CGFloat = 0;
+        for font in fonts {
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 250 + i))
+            label.textAlignment = .center
+            label.attributedText = NSAttributedString(string: font)
+            label.font = UIFont(name: font, size: 25)
+            self.view.addSubview(label)
+            i += 100;
+        }
+        let boldDescriptor = UIFontDescriptor(fontAttributes: [
+            UIFontDescriptor.AttributeName.family: "Avenir Next",
+            UIFontDescriptor.AttributeName.traits: [
+                UIFontDescriptor.TraitKey.weight: UIFont.Weight.bold
+            ]
+        ])
+        print(boldDescriptor)
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.width + i))
+        label.textAlignment = .center
+        label.attributedText = NSAttributedString(string: "Avenir Next")
+        label.font = UIFont(descriptor: boldDescriptor, size: 25)
+        self.view.addSubview(label)
     }
 
 
